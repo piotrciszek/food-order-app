951092a68d4226b650a7fec0b03c5663d193778a
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2021-08-28 14:27:08 +0200

    meals summary and dummy-meals list added

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,16 @@
+import { fragment } from 'react';
+
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+import React, { Fragment } from "react";
+
+const Meals = () => {
+    return (
+        <Fragment>
+        <MealsSummary />
+        <AvailableMeals />
+        </Fragment>
+    );
+};
+
+export default Meals;
