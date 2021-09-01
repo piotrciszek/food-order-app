359fd4a681f8507849549b1c113b02afa466c083
Piotr Ciszek <piotr_ciszek@poczta.onet.pl>
2021-09-01 22:41:34 +0200

    adding modal

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Fragment } from 'react';
+import { Fragment } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
       </main>
-      
     </Fragment>
-
   );
 }
 
